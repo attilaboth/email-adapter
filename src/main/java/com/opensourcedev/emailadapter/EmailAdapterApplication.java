d3c630321e0483d288bf613c69686df8a93759d7
Šimon <sajmon23252gmail.com>
2020-01-14 20:55:08 +0100

    Initial project setup

---
+++ src/main/java/com/opensourcedev/emailadapter/EmailAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.emailadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensourcedev/emailadapter/EmailAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.emailadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
