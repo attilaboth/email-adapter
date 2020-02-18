96dadcc349415637145126ff895c6cb801907cbb
Šimon <sajmon23252gmail.com>
2020-02-18 21:14:02 +0100

    Successfully implemented all methods

--- src/main/java/com/opensourcedev/emailadapter/service/email_service/EmailHandler.java
+++ src/main/java/com/opensourcedev/emailadapter/service/email_service/EmailHandler.java
@@ -1,10 +1,32 @@
 package com.opensourcedev.emailadapter.service.email_service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Slf4j
 public class EmailHandler {
+
+    private CheckEmail checkEmail;
+    private FetchEmail fetchEmail;
+
+    @Autowired
+    public EmailHandler(CheckEmail checkEmail, FetchEmail fetchEmail) {
+        this.checkEmail = checkEmail;
+        this.fetchEmail = fetchEmail;
+    }
+
+    private void checkCheckEmails(boolean folderLookup){
+        log.debug("[*] checkCheckEmails() method has been called with \"folderLookup\" parameter set to: " + folderLookup);
+        checkEmail.checkAllMailsInMailbox(folderLookup);
+    }
+
+    private void fetchEmails(boolean folderLookup){
+        log.debug("[*] fetchEmails() method has been called with \"folderLookup\" parameter set to: " + folderLookup);
+        fetchEmail.fetchMailsInMailbox(folderLookup);
+    }
 }
