2118f4b838e15296a9d5015ff8bba23813ddb409
Šimon <sajmon23252gmail.com>
2020-02-23 21:39:34 +0100

    Small updates

--- src/main/java/com/opensourcedev/emailadapter/service/email_service/EmailHandler.java
+++ src/main/java/com/opensourcedev/emailadapter/service/email_service/EmailHandler.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.Message;
 import java.util.HashSet;
